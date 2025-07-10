e13dccbfefc32050beca478bd0dc8f6d6fe9ba75
jose.vfonseca <jose.vfonseca@luizalabs.com>
2025-07-10 00:26:29 -0300

    fix: add example get users

--- api/entity_example/dto/response.go
+++ api/entity_example/dto/response.go
@@ -8,3 +8,12 @@ type FindEntityExampleResponse struct {
 type FindAllEntityExamplesResponse struct {
 	Name string `json:"name" example:"fulano"`
 }
+
+type PreferenceResponse struct {
+	Description string `json:"description"`
+}
+type UserResponse struct {
+	Name        string               `json:"name"`
+	BirthDate   string               `json:"birth_date"`
+	Preferences []PreferenceResponse `json:"preferences"`
+}
